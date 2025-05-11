26b25a211a76244da76dc0f996e7af8c832db5d0
MohammedAmalAlRaihan <orangtan3422@gmail.com>
2025-05-11 12:58:06 +1000

    updated credit

--- src/components/Credits/Credits.jsx
+++ src/components/Credits/Credits.jsx
@@ -1,19 +1,33 @@
 import './credits.css';
+import ExpandedCredits from './ExpandedCredits.jsx';
 
 function Credits() {
+    function EnableOverlay() {
+        const blackout = document.querySelector('.blackout');
+        if (blackout) {
+            blackout.style.display = 'flex';
+        }
+    }
+
     return (
-        <section className='credits-section'>
-            <b className='title'>Karma Farm Completion List</b>
-            <br />
-            <p className='description'>a collection of the hardest levels completed by the Geometry Dash Karma Farm Team </p>
-            <br /><br />
-            <div className='text-con'>
-                <span>Website developed by <b>ORANGETAN</b> </span> <hr /> <br /> 
-                <span>Original list organised by <b>Henabidus</b></span> <hr /> <br />
-                <span>Karma Farm hosted by <b>Crackermanuel</b></span> <hr />
-            </div>
-            <a href='https://github.com/ORANGETAN3422/KFList' target='_blank'><img src={`${import.meta.env.BASE_URL}github.png`} className='gh-img' /></a>
-        </section>
+        <>
+            <ExpandedCredits />
+            <section className='credits-section'>
+                <b className='title'>Karma Farm Completion List</b>
+                <br />
+                <p className='description'>a collection of the hardest levels completed by the Geometry Dash Karma Farm Team </p>
+                <br /><br />
+                <div className='text-con'>
+                    <span>Website developed by <b>ORANGETAN</b> </span> <hr /> <br />
+                    <span>Original list organised by <b>Henabidus</b></span> <hr /> <br />
+                    <span>Karma Farm hosted by <b>Crackermanuel</b></span> <hr />
+                </div>
+                <div className='more-credits'>
+                    <img src={`${import.meta.env.BASE_URL}more-icon.png`} className='more-img' onClick={() => EnableOverlay()}/>
+                    <a href='https://github.com/ORANGETAN3422/KFList' target='_blank'><img src={`${import.meta.env.BASE_URL}github.png`} className='gh-img' /></a>
+                </div>
+            </section>
+        </>
     )
 }
 
---
+++ src/components/Credits/ExpandedCredits.jsx
@@ -0,0 +1,29 @@
+
+
+function ExpandedCredits() {
+    function DisableOverlay() {
+        const blackout = document.querySelector('.blackout');
+        if (blackout) {
+            blackout.style.display = 'none';
+        }
+    }
+
+    return (
+        <div className='blackout'>
+            <div className='expanded-con'>
+                <button className="expanded-con-button" value='back' onClick={() => DisableOverlay()} > Back </button>
+                <h3 className="extras-title">Other</h3>
+                <div className="extra-text-con">
+                    <hr />
+                    Extreme Demon Rankings provided by <a href="https://aredl.net" target="_blank">AREDL</a> <br />
+                    Demon Tiers provided by <a href="https://gdladder.com/" target="_blank">GDDL</a> <br />
+                    Level thumbnails provided by cdc's <a href='https://github.com/cdc-sys/level-thumbnails' target="_blank">Level Thumbnails Mod</a> <br />
+                    <br />
+                    Made in <a href="https://react.dev/" target="_blank">React</a> + <a href="https://vite.dev/" target="_blank">Vite</a> 
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ExpandedCredits
